4cb7b3366dad6a4f2d10aa78ec1756615ad82587
mandarinrock <mandarinrock.dg@gmail.com>
2022-08-30 20:28:25 -0700

    Initial Commit

---
+++ copilot_example.py
@@ -0,0 +1,23 @@
+
+def printBoard(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            print(board[i][j], end=' ')
+        print()
+
+def playTicTacToe():
+    print("Tic Tac Toe")
+    print("---------")
+    print("")
+    
+
+    
+
+def main():
+    print("Hello World")
+
+    playTicTacToe()
+    print("Terminating")
+
+
+main()
